5068d9a619ebb2002dbee9013f14756d0dc9fd07
Jaden <app@jadenmail.com>
2017-10-12 14:02:31 +0800

    3-8

--- imooc-security-demo/src/main/java/com/jadendong/DemoApplication.java
+++ imooc-security-demo/src/main/java/com/jadendong/DemoApplication.java
@@ -6,16 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	
-	@GetMapping("/hello")
-	public String hello() {
-		return "Hello";
-	}
-	
 }
---
+++ imooc-security-demo/src/main/java/com/jadendong/web/aspect/TimApsect.java
@@ -0,0 +1,33 @@
+package com.jadendong.web.aspect;
+
+import java.util.Date;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class TimApsect {
+	
+	@Around("execution(* com.jadendong.web.controller.UserController.*(..))")
+	public Object handleControllerMethod(ProceedingJoinPoint pjp) throws Throwable {
+		System.out.println("Aspect start");
+		Object[] args=pjp.getArgs();
+		
+		for (Object arg : args) {
+			System.out.println("args is "+arg);
+		}
+		
+		long start=new Date().getTime();
+		
+		Object object=pjp.proceed();
+		
+		System.out.println("time aspect 耗时："+(new Date().getTime()-start));
+		System.out.println("aspect end");
+		
+		return object;
+	}
+	
+}
